a061ffa9e392b2ef828386e2758814ee7c39c9ed
Brian Chuk <brian_chuk@yahoo.com>
2014-11-15 19:10:27 -0500

    initial code

---
+++ Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+#include "Leap.h"
+
+using namespace Leap;
+
+int main(int argc, char** argv) {
+
+    // Keep this process running until Enter is pressed
+    std::cout << "Press Enter to quit..." << std::endl;
+    std::cin.get();
+
+    return 0;
+}
