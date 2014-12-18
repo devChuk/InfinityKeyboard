216505869759171873239b16ad3d41abcacd4dfa
Brian Chuk <brian_chuk@yahoo.com>
2014-12-17 23:21:48 -0500

    organizing files and progress

--- versionOne.cpp
+++ versionOne.cpp
@@ -17,6 +17,25 @@ struct word {
     string w;
 };
 
+class poo { //can't think of a class name. This is the class that takes words typed in a 1L format and returns a list of 10 predicted English words
+public:
+	poo() { //initialize the head treeNode
+		head.data = "";
+		head.isEndOfString = 1;
+		head.branches.push_back(NULL);
+	}
+private:
+	Node head;
+};
 
+int main() {
+	poo wordGuess;
+}
 /*
-Steps of main function
+Steps of main functionu
+
+1)create initial head
+2)cin for word input
+3)grow dat tree
+
+*/
