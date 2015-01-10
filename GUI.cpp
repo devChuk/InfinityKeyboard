d99fa61622893e0c32e65b9ff8ceade83080ea1d
Brian Chuk <brian_chuk@yahoo.com>
2015-01-10 00:55:57 -0500

    programming during a cruise is actually very nice

---
+++ GUI.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <allegro.h>
+using namespace std;
+
+
+
+int main(){
+ 
+    allegro_init();
+    install_keyboard();
+    set_gfx_mode( GFX_AUTODETECT, 640, 480, 0, 0);
+    
+    readkey();
+    
+    return 0;
+    
+}   
+END_OF_MAIN(); 
