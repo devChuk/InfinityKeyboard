655d672a7ba0e9dd50e555d074b9311dc8078cf7
Brian Chuk <brian_chuk@yahoo.com>
2015-01-29 14:20:34 -0500

    loading images

--- GUI.cpp
+++ GUI.cpp
@@ -1,47 +1,121 @@
+//SDL image needed
+
+//Using SDL and standard IO
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <stdio.h>
 
 //Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1920;
+const int SCREEN_HEIGHT = 1080;
 
-int main(int argc, char* args[]) { //SDL requires this type of main so that it's compatible with multiple platforms
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
 	
-	//Just a 2D image. Can be loaded or be the image inside of a window
-	SDL_Surface* screenSurface = NULL;
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
 
 	//Initialize SDL
-	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
 		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
 	}
-	else {
+	else
+	{
 		//Create window
-		window = SDL_CreateWindow("Infinity Keyboard", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if(window == NULL) {
+		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
 			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
 		}
-		else {
+		else
+		{
 			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-			
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Wait two seconds
-			SDL_Delay(2000);
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
 		}
 	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load splash image
+	gHelloWorld = IMG_Load( "./gradientbg.jpg" );
+	if( gHelloWorld == NULL )
+	{
+		printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface( gHelloWorld );
+	gHelloWorld = NULL;
 
 	//Destroy window
-	SDL_DestroyWindow(window);
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
 
 	//Quit SDL subsystems
 	SDL_Quit();
+}
+
+int main( int argc, char* args[] )
+{
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+		{
+			//Apply the image
+			SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+			
+			//Update the surface
+			SDL_UpdateWindowSurface( gWindow );
+
+			//Wait two seconds
+			SDL_Delay( 2000 );
+		}
+	}
+
+	//Free resources and close SDL
+	close();
 
 	return 0;
 }
